ebb740ce7bc3a9be2976ec41dbcfc470875b9213
srinath131289 <161568833+srinath131289@users.noreply.github.com>
2025-01-10 09:10:38 +0800

    Delete 01_Basics directory

--- 01_Basics/variable.js
+++
@@ -1,7 +0,0 @@
-const acctId = 234567
-let acctName = "Srinath"
-let acctlocation = "Singapore"
-var addressPincode = "520241"
-let phonenum ;
-
-console.table([acctId, acctName, acctlocation, addressPincode, phonenum])
